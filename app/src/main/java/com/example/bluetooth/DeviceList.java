34ceee428e0ec8bf833c38b0892a14c8dfe3e289
MaidOf-Atoms <105539336+MaidOf-Atoms@users.noreply.github.com>
2023-01-21 01:18:45 +0400

    Initial commit

---
+++ app/src/main/java/com/example/bluetooth/DeviceList.java
@@ -0,0 +1,14 @@
+package com.example.bluetooth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeviceList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+    }
+}
---
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.bluetooth;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.concurrent.BlockingDeque;
+
+public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private BluetoothAdapter bluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this;
+        initBluetooth();
+    }
+
+    private void initBluetooth() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(context, "No bluetooth", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_serch:
+                Toast.makeText(context, "Search Devices", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_on_off:
+                enableBluetooth();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void enableBluetooth() {
+        if (bluetoothAdapter.isEnabled()) {
+            Toast.makeText(context, "Bluetooth found", Toast.LENGTH_SHORT).show();
+        } else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            bluetoothAdapter.enable();
+        }
+    }
+}
