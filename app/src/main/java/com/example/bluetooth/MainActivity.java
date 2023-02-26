3844f3ff122d51ade54ad305128e9631b1cfb3e1
MaidOf-Atoms <105539336+MaidOf-Atoms@users.noreply.github.com>
2023-02-26 14:47:32 +0400

    Comit 1

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -61,9 +61,18 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(context, "Bluetooth found", Toast.LENGTH_SHORT).show();
         } else {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
                 return;
+            }else {
+                bluetoothAdapter.enable();
             }
-            bluetoothAdapter.enable();
+
         }
     }
 }
